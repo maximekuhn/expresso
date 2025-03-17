24f6d07d8a1eaaba23cc2c39c1a00f0c9170031e
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2025-03-17 21:55:57 +0100

    initial commit

--- internal/transaction/session.go
+++ internal/transaction/session.go
@@ -3,5 +3,16 @@ package transaction
 import "context"
 
 type Session interface {
+	// Transaction executes the function f in an atomic way. If f returns an error,
+	// the transaction is rollbacked; otherwise it is committed.
+	//
+	// Usage example:
+	//
+	//  session := SessionProvider.Provide() // get a new session
+	//  ctx := context.TODO()
+	//  session.Transaction(ctx, func(ctx context.Context) error {
+	//      // ...
+	//      return nil
+	//  })
 	Transaction(ctx context.Context, f func(ctx context.Context) error) error
 }
